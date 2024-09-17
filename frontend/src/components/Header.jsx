60e717193cd9a5d1eaf151eec3a4ee34f1ba395d
Hussain1236 <159534219+Hussain1236@users.noreply.github.com>
2024-09-17 13:01:35 +0100

    Add files via upload
    push to github

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import logo from '../assets/baze_logo.png'
+
+const Header = () => {
+  return (
+    <div className="flex items-center justify-between p-5">
+      <div className="w-[10%]">
+        <img src={logo} alt="" className="w-full" />
+      </div>
+      <div className="flex items-center gap-4 ">
+        <Link className="text-white hover:text-gray-300" to="/">Home</Link>
+        <Link className="text-white hover:text-gray-300" to="/about">About</Link>
+        <Link className="text-white hover:text-gray-300" to="/about">Services</Link>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
